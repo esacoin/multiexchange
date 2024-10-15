b73325bec5c4b9527acd6c72352ea2f01dda9574
esacoin <esacoins@gmail.com>
2024-10-15 20:41:55 +0200

    cleanup image

--- src/components/illustrations/JumperLogo.tsx
+++ src/components/illustrations/JumperLogo.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-
 export const JumperLogo = () => {
   return (
     <>
       <img 
         alt="Esacoin Logo" 
         src="https://esa.co.in/wp-content/uploads/2024/10/esacoinLogo.png" 
-        width="200"  // Adjust as needed
-        height="100" // Adjust as needed
       />
     </>
   );
